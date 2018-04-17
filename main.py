8f282d92e44713430f162523c5b55280b4e8d654
karol <karol.kowalski95@gmail.com>
2018-04-17 19:59:26 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,140 @@
+"""
+ Example program to show using an array to back a grid on-screen.
+
+ Sample Python/Pygame Programs
+ Simpson College Computer Science
+ http://programarcadegames.com/
+ http://simpson.edu/computer-science/
+
+ Explanation video: http://youtu.be/mdTeqiWyFnc
+"""
+import pygame
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 10
+HEIGHT = 10
+
+SIZE = 127
+
+window_width = 800
+window_height = 600
+
+map_width = 2540
+map_height = 2540
+
+# This sets the margin between each cell
+MARGIN = 5
+
+# Create a 2 dimensional array. A two dimensional
+# array is simply a list of lists.
+grid = []
+for row in range(SIZE):
+    # Add an empty array that will hold each cell
+    # in this row
+    grid.append([])
+    for column in range(SIZE):
+        grid[row].append(0)  # Append a cell
+
+# Set row 1, cell 5 to one. (Remember rows and
+# column numbers start at zero.)
+grid[1][5] = 1
+
+# Initialize pygame
+pygame.init()
+
+# Set the HEIGHT and WIDTH of the screen
+WINDOW_SIZE = [window_width,  window_height]
+screen = pygame.display.set_mode(WINDOW_SIZE)
+main_map = pygame.Surface((map_width, map_height))
+main_map = main_map.convert()
+
+# Set title of screen
+pygame.display.set_caption("Array Backed Grid")
+
+# Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+map_x = 0  # Only this should change
+map_y = 0
+# The change for x
+map_x_c = -3
+screen.blit(main_map, (map_x, map_y, window_width, window_height))
+pygame.display.flip()
+print(map_x, map_y, map_x + window_width, map_y + window_height)
+
+def draw():
+    for row in range(SIZE):
+        for column in range(SIZE):
+            color = WHITE
+            if grid[row][column] == 1:
+                color = GREEN
+            pygame.draw.rect(main_map,
+                             color,
+                             [(MARGIN + WIDTH) * column + MARGIN,
+                              (MARGIN + HEIGHT) * row + MARGIN,
+                              WIDTH,
+                              HEIGHT])
+    pygame
+
+
+# -------- Main Program Loop -----------
+while not done:
+    key_pressed = pygame.key.get_pressed()
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:  # and map_x != 0:
+                map_x -= map_x_c
+                print(map_x, map_y, map_x + window_width, map_y + window_height)
+                draw()
+            elif event.key == pygame.K_RIGHT:
+                map_x += map_x_c
+                print(map_x, map_y, map_x + window_width, map_y + window_height)
+                draw()
+        # elif key_pressed[K_UP]:  # and map_x != 0:
+        #     map_y -= map_x_c
+        #     print(map_x, map_y, map_x + window_width, map_y + window_height)
+        #     # draw()
+        #     # ucircle = pygame.draw.circle(main_map, orange, ((((-map_x+window_width)/2)+(-(map_x)/2)), (((-map_y+window_width)/2)+(-(map_y)/2))), 20)
+        # elif key_pressed[K_DOWN]:  # and map_x != 2000:
+        #     map_y += map_x_c
+        #     print(map_x, map_y, map_x + window_width, map_y + window_height)
+        #     # draw()
+        #     # ucircle = pygame.draw.circle(main_map, orange, ((((-map_x+window_width)/2)+(-(map_x)/2)), (((-map_y+window_width)/2)+(-(map_y)/2))), 20)
+        # elif key_pressed[K_ESCAPE]:
+        #     quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # User clicks the mouse. Get the position
+            pos = pygame.mouse.get_pos()
+            # Change the x/y screen coordinates to grid coordinates
+            column = (pos[0] - map_x) // (WIDTH + MARGIN)
+            row = pos[1] // (HEIGHT + MARGIN)
+            # Set that location to one
+            grid[row][column] = 1
+            print("Click ", pos, "Grid coordinates: ", row, column)
+
+    # Set the screen background
+    main_map.fill(BLACK)
+
+    # Draw the grid
+    draw()
+    screen.blit(main_map, (map_x, map_y, window_width, window_height))
+    # Limit to 60 frames per second
+    clock.tick(60)
+
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+# Be IDLE friendly. If you forget this line, the program will 'hang'
+# on exit.
+pygame.quit()
---
+++ test.py
@@ -0,0 +1,84 @@
+# Attempt at scrolling
+import pygame
+from pygame.locals import *
+
+pygame.init()
+# All colors used
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+orange = (255, 100, 0)
+purple = (255, 0, 255)
+black = (0, 0, 0)
+white = (255, 255, 255)
+colors = [green, blue, orange, purple, white]
+# Initialize the screen you see
+window_width = 600
+window_height = 480
+screen = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Scrolling Attempt!")
+# Draw the main map
+# (Draw what you want here)
+map_width = 2000
+map_height = 700
+main_map = pygame.Surface((map_width, map_height))
+main_map = main_map.convert()
+
+
+def draw():
+    main_map.fill(black)
+    rect1 = pygame.draw.rect(main_map, red, (250, 40, 1700, 400))
+    circle1 = pygame.draw.circle(main_map, white, (1000, 300), 20)
+    circle1 = pygame.draw.circle(main_map, green, (1000, 200), 20)
+    circle1 = pygame.draw.circle(main_map, blue, (1000, 100), 20)
+
+
+draw()
+# The coords you see
+map_x = 0  # Only this should change
+map_y = 0
+# The change for x
+map_x_c = -3
+screen.blit(main_map, (map_x, map_y, window_width, window_height))
+pygame.display.flip()
+print(map_x, map_y, map_x + window_width, map_y + window_height)
+ucircle = pygame.draw.circle(screen, orange, (10,10), 20)
+# The program
+while True:
+    for event in pygame.event.get():
+        pass
+    key_pressed = pygame.key.get_pressed()
+    if key_pressed[K_LEFT]:  # and map_x != 0:
+        map_x -= map_x_c
+        print(map_x, map_y, map_x + window_width, map_y + window_height)
+        # draw()
+        # ucircle = pygame.draw.circle(main_map, orange, ((((-map_x+window_width)/2)+(-(map_x)/2)), (((-map_y+window_width)/2)+(-(map_y)/2))), 20)
+    if key_pressed[K_RIGHT]:  # and map_x != 2000:
+        map_x += map_x_c
+        print(map_x, map_y, map_x + window_width, map_y + window_height)
+        # draw()
+        # ucircle = pygame.draw.circle(main_map, orange, ((((-map_x+window_width)/2)+(-(map_x)/2)), (((-map_y+window_width)/2)+(-(map_y)/2))), 20)
+    if key_pressed[K_UP]:  # and map_x != 0:
+        map_y -= map_x_c
+        print(map_x, map_y, map_x + window_width, map_y + window_height)
+        # draw()
+        # ucircle = pygame.draw.circle(main_map, orange, ((((-map_x+window_width)/2)+(-(map_x)/2)), (((-map_y+window_width)/2)+(-(map_y)/2))), 20)
+    if key_pressed[K_DOWN]:  # and map_x != 2000:
+        map_y += map_x_c
+        print(map_x, map_y, map_x + window_width, map_y + window_height)
+        # draw()
+        # ucircle = pygame.draw.circle(main_map, orange, ((((-map_x+window_width)/2)+(-(map_x)/2)), (((-map_y+window_width)/2)+(-(map_y)/2))), 20)
+    if key_pressed[K_ESCAPE]:
+        quit()
+    if map_x > 0:
+        map_x = 0
+    if map_x < -(map_width - window_width):
+        map_x = -(map_width - window_width)
+
+    if map_y > 0:
+        map_y = 0
+    if map_y < -(map_height - window_height):
+        map_y = -(map_height - window_height)
+    screen.blit(main_map, (map_x, map_y, window_width, window_height))
+    ucircle = pygame.draw.circle(screen, orange, (10,10), 20)
+    pygame.display.flip()
